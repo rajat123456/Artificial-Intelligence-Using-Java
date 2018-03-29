f87ea6a176435f183245ecbaf70336ca22e453af
Rajat Rawat <rajatrawat1997@gmail.com>
2018-03-29 18:01:01 +0530

    Add files via upload

---
+++ Meta Heuristic Optimization Algorithms/Travelling Salseman Problem/App.java
@@ -0,0 +1,18 @@
+package tsp;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		for(int i=0;i<5;i++) {
+			City city = new City();
+			Repository.addCity(city);
+		}
+			
+		SimulatedAnnealing annealing = new SimulatedAnnealing();
+		annealing.simulation();
+		
+		System.out.println("Final approximated solution's distance is: " + annealing.getBest().getDistance());
+		System.out.println("Tour: " + annealing.getBest());
+	}
+}
