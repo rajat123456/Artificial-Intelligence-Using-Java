6c4f3a36fbb11eef3bc2d00dbf108dde6db1169b
Rajat Rawat <rajatrawat1997@gmail.com>
2018-03-29 16:20:26 +0530

    Add files via upload

---
+++ Meta Heuristic Optimization Algorithms/Simulated Annealing/App.java
@@ -0,0 +1,11 @@
+package sa1;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		SimulatedAnnealing annealing = new SimulatedAnnealing();
+		annealing.findOptimum();
+		
+	}
+}
